eaca79dc5e35f605cf11081fed2da92e77ed2f5e
donridley1972 <45693640+donridley1972@users.noreply.github.com>
2022-10-20 11:11:21 -0500

    Add files via upload

---
+++ Visual Studio Solution/Form1.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace ClarionToDotNet
+{
+    public partial class Form1 : Form
+    {
+        [DllImport(@"ClarionToDotNetDLL.dll")]
+        public static extern void HelloFromClarion();
+
+        [DllImport(@"ClarionToDotNetDLL.dll")]
+        public static extern void BrowseClasses();
+
+        [DllImport(@"ClarionToDotNetDLL.dll")]
+        public static extern void BrowseStudents();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HelloFromClarion();
+            //CallHelloFromClarion();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            BrowseClasses();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            BrowseStudents();
+        }
+    }
+}
